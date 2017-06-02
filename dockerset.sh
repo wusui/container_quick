dab77fad7dee144d4a004cdee988cbfaa5496792
Warren Usui <wusui@users.noreply.github.com>
2017-06-02 14:28:17 -0700

    First version

--- dockerset.sh
+++ dockerset.sh
@@ -1,1 +1,5 @@
-
+#! /bin/bash -fv
+yum install docker -y
+service docker start
+sed -i -e "s|.*INSECURE_REGISTRY=.*|INSECURE_REGISTRY='--insecure-registry brew-pulp-docker01.web.prod.ext.phx2.redhat.com:8888'|" /etc/sysconfig/docker
+service docker restart
